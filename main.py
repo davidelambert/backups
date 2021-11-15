7b2dbf56564d62ae3eb6c7e8555623243b184612
David E. Lambert <david@davidelambert.com>
2021-11-15 17:06:34 -0500

    revisions to backup()
    remove title param => retrieve last word of source path
    cleaner log path
    rsync-native features to retain old files by date

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from pathlib import Path
-import os
-import shutil
 import subprocess
 import json
 from datetime import date
@@ -8,21 +6,22 @@ from datetime import date
 conf_dir = Path('./conf')
 today = date.today()
 
-with open(conf_dir/'config.json', 'r') as s:
-    config = json.load(s)
+with open(conf_dir/'config.json', 'r') as c:
+    config = json.load(c)
 log_dir = Path(config['log_dir'])
 b = config['backups']
 test = b['test']
 
-def backup(entry:dict, title:str):
+
+def backup(entry:dict):
     """Takes an entry from the parsed JSON settings
     (settings['backups']), processes the entry's values,
     and passes processed values as arguments to a system-executed
     rsync command via subprocess.call().
     """
 
-    source = entry['source']
-    dest = entry['dest'] + '/' + title + '_' + str(today)
+    # last word of source path, excluding trailing slashes
+    src_name = entry['source'].strip('/').split('/')[-1]
 
     if 'excludes' in entry.keys() and entry['excludes'] is not None:
         excludes = str(Path(conf_dir/entry['excludes']).absolute())
@@ -30,17 +29,20 @@ def backup(entry:dict, title:str):
         excludes = ''
 
     if 'log' in entry.keys() and entry['log'] is True:
-        log = str(log_dir.absolute()) + '/' + title + '_' + str(today) + '.log'
+        log = f'{log_dir.absolute()}/{src_name}_{today}.log'
     else:
         log = ''
 
-    subprocess.call(['rsync', '-az',
+    subprocess.call(['rsync', '-abCz', '--delete-before',
+                    '--backup-dir=archive_`date +%F`',
+                    '--exclude=archive_*',
                     f'--exclude-from={excludes}',
                     f'--log-file={log}',
-                    source, dest])
+                    entry['source'],
+                    entry['dest']])
 
 
-backup(test, '.jupyter')
+backup(test)
 
 
 # for name, data in b.items():
