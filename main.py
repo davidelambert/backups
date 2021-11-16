e5850d98fc9596d03a57bf3e3324e2342e8c9d8d
David E. Lambert <david@davidelambert.com>
2021-11-16 12:41:07 -0500

    add  basic cleanup() FN calling ssh

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import subprocess
 import json
-from datetime import date
+from datetime import date, timedelta
 
 conf_dir = Path('./conf')
 today = date.today()
@@ -23,6 +23,9 @@ def backup(entry:dict):
     # last word of source path, excluding trailing slashes
     src_name = entry['source'].strip('/').split('/')[-1]
 
+    
+    #TODO: more input validation
+
     if 'excludes' in entry.keys() and entry['excludes'] is not None:
         excludes = str(Path(conf_dir/entry['excludes']).absolute())
     else:
@@ -33,6 +36,7 @@ def backup(entry:dict):
     else:
         log = ''
 
+    # TODO: file size limit
     subprocess.call(['rsync', '-abCz', '--delete-before',
                     '--backup-dir=archive_`date +%F`',
                     '--exclude=archive_*',
@@ -42,7 +46,25 @@ def backup(entry:dict):
                     entry['dest']])
 
 
-backup(test)
+def cleanup(entry:dict):
+    """Deletes the destination archive directory (containing
+    previous versions of files synced by backup()) dated _int_
+    days ago, where _int_ is read from the config.json entry's
+    'retain_days' key, if present."""
+
+    #TODO: input validation
+    
+    cleanup_date = today - timedelta(days=entry['retain_days'])
+    dest_server = entry['dest'].split(':')[0]
+    dest_path = entry['dest'].split(':')[-1]
+
+    subprocess.call(['ssh', dest_server, 'rm', '-rfv',
+                     f'{dest_path}archive_{cleanup_date}/'])
+
+    #TODO: log rm output
+
+
+
 
 
 # for name, data in b.items():
