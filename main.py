275d121d656244309707efdfbba1b9b634a0b299
David E. Lambert <david@davidelambert.com>
2021-11-16 16:24:23 -0500

    parse dest_* for remote/local cases

--- main.py
+++ main.py
@@ -11,17 +11,24 @@ with open(conf_dir/'config.json', 'r') as c:
 
 
 def backup(entry:dict):
-    """Takes an entry from the parsed JSON settings
-    (settings['backups']), processes the entry's values,
-    and passes processed values as arguments to a system-executed
-    rsync command via subprocess.call().
+    """Takes an entry from config.json and passes processed
+    entry values as arguments to an rsync subprocess.
     """
 
     # last word of source path, excluding trailing slashes
-    src_name = entry['source'].strip('/').split('/')[-1]
+    src_name = entry['src'].strip('/').split('/')[-1]
 
     
     #TODO: more input validation
+
+    if 'dest_server' in entry.keys() and entry['dest_server'] is not None:
+        if 'dest_user' in entry.keys() and entry['dest_user'] is not None:
+            user = f"{entry['dest_user']}@"
+        else:
+            user = ''
+        remote = f"{user}{entry['dest_server']}:"
+    else:
+        remote = ''
 
     if 'excludes' in entry.keys() and entry['excludes'] is not None:
         excludes = str(Path(conf_dir/entry['excludes']).absolute())
@@ -39,31 +46,44 @@ def backup(entry:dict):
                     '--exclude=archive_*',
                     f'--exclude-from={excludes}',
                     f'--log-file={log}',
-                    entry['source'],
-                    entry['dest']])
+                    entry['src'],
+                    f"{remote}{entry['dest_path']}"])
 
 
 def cleanup(entry:dict):
     """Deletes the destination archive directory (containing
-    previous versions of files synced by backup()) dated _int_
-    days ago, where _int_ is read from the config.json entry's
-    'retain_days' key, if present."""
+    previous versions of files synced by backup()) dated {int}
+    days ago. {int} is read from the config.json entry's
+    'retain_days' key, if present, and defaults to 7 otherwise.
+    """
 
-    #TODO: input validation
-    
+    # target archive directory to delete
+    if 'retain_days' in entry.keys() and entry['retain_days'] is not None:
+        cleanup_date = date.today() - timedelta(days=entry['retain_days'])
+    else:
+        cleanup_date = date.today() - timedelta(days=7)
+    cleanup_target = f"{entry['dest_path']}archive_{cleanup_date}/"
 
-    cleanup_date = date.today() - timedelta(days=entry['retain_days'])
-    dest_server = entry['dest'].split(':')[0]
-    dest_path = entry['dest'].split(':')[-1]
+    # user/server if destination is remote
+    if 'dest_server' in entry.keys() and entry['dest_server'] is not None:
+        if 'dest_user' in entry.keys() and entry['dest_user'] is not None:
+            user = f"{entry['dest_user']}@"
+        else:
+            user = ''
+        remote = f"{user}{entry['dest_server']}"
+    else:
+        remote = ''
+
+    # delete remote/local target directory
+    if remote != '':
+        subprocess.call(['ssh', remote, 'rm', '-rfv', cleanup_target])
+    else:
+        subprocess.call(['rm', '-rfv', cleanup_target])
 
 
     #TODO: log rm output
-    src_name = entry['source'].strip('/').split('/')[-1]
-    log = f'{log_dir.absolute()}/{src_name}_{date.today()}.log'
-
-
-    subprocess.call(['ssh', dest_server, 'rm', '-rfv',
-                     f'{dest_path}archive_{cleanup_date}/'])
+    # src_name = entry['src'].strip('/').split('/')[-1]
+    # log = f'{log_dir.absolute()}/{src_name}_{date.today()}.log'
 
 
 test = config['test']
