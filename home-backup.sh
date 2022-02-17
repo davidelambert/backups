c6adb9105486a1bc7db79b607e47df1701e757b4
David E. Lambert <david@davidelambert.com>
2022-02-17 13:12:58 -0500

    scalar retention days removes $YESTERWEEK/$YESTERMONTH replaces with a single $RETAIN_DAYS variable deletion of remote backup directories & local logs both reference $RETAIN_DAYS

--- home-backup.sh
+++ home-backup.sh
@@ -16,11 +16,11 @@ DEST=$REMOTE_DIR/current
 
 TODAY=$(date "+%F")
 YESTERDAY=$(date "+%F" -d "1 day ago")
-YESTERWEEK=$(date "+%F" -d "1 week ago")
-YESTERMONTH=$(date "+%F" -d "1 month ago")
 
+RETAIN_DAYS=14
+RETAIN_DATE=$(date "+%F" -d "$RETAIN_DAYS days ago")
 RETAIN_DIR=$REMOTE_DIR/.backup_$YESTERDAY
-RETAIN_DELETE=$REMOTE_DIR/.backup_$YESTERWEEK
+RETAIN_DELETE=$REMOTE_DIR/.backup_$RETAIN_DATE
 
 EXCLUDES=$HOME/backup_excludes
 
@@ -38,13 +38,15 @@ if [ ! -d "$LOG_DIR" ] ; then
     mkdir -p $LOG_DIR
 fi
 
-# delete log from 1 month ago
-if [ -f "$LOG_DIR/$YESTERMONTH.log" ] ; then
-    rm -f "$LOG_DIR/$YESTERMONTH.log"
+# delete log after retention period
+if [ -f "$LOG_DIR/$RETAIN_DATE.log" ] ; then
+    rm -f "$LOG_DIR/$RETAIN_DATE.log"
 fi
 
-# delete backup dir from 1 week ago
+# delete backup dir after retention period
 ssh $SERVER "[[ -d '$RETAIN_DELETE' ]] && rm -rf $RETAIN_DELETE"
 
 # abbreviated command
+echo 'Working...'
 rsync $ARGS $SOURCE $SERVER:$DEST
+echo 'Done.'
