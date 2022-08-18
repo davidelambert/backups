77b3db9c1c6432b55a719ee2d07b262cf13e3e69
David E. Lambert <david@davidelambert.com>
2022-08-18 17:08:22 -0400

    begin 'new' version
    simplified version, backing up all of hotrod:~

--- home-backup.sh
+++ home-backup.sh
@@ -7,45 +7,19 @@
 # @daily    30  home-backup.daily   /full/path/to/script
 # ==============================================================
 
-PROJECT_ROOT=$HOME/projects/backups
+SERVER=hotrod.lan
+SRC=~
+DST=~/backup
+EXCLUDES=./backup_excludes
 
-SOURCE=$HOME/
-SERVER=clunker.lan
-REMOTE_DIR=$HOME/hotrod-backup
-DEST=$REMOTE_DIR/current
-
-
-TODAY=$(date -I)
-YESTERDAY=$(date -I -d "1 day ago")
-
-RETAIN_DAYS=14
-RETAIN_DATE=$(date -I -d "$RETAIN_DAYS days ago")
-RETAIN_DIR=$REMOTE_DIR/.backup_$YESTERDAY
-RETAIN_DELETE=$REMOTE_DIR/.backup_$RETAIN_DATE
-
-EXCLUDES=$HOME/backup_excludes
-
-LOG_DIR=$HOME/.log/home-backup
-
-ARGS="-hhaz --stats
+ARGS="-hhazi --backup
+      --backup-dir=$HOME/backup/archive
       --delete --delete-excluded
-      --force --ignore-errors 
       --exclude-from=$EXCLUDES
-      --backup --backup-dir=$RETAIN_DIR
-      --log-file=$LOG_DIR/$TODAY.log"
+      --log-file=./test.log"
 
-# create log & backup directories if they don't exist
-[[ ! -d "$LOG_DIR" ]] && mkdir -p $LOG_DIR
-ssh $SERVER "[[ ! -d '$REMOTE_DIR' ]] && mkdir -p $REMOTE_DIR"
+rsync $ARGS $SERVER:$SRC $DEST & job=$!
 
-# delete logs & backups older than retention period
-$PROJECT_ROOT/delete_logs.sh "$LOG_DIR" "$RETAIN_DATE"
-ssh $SERVER "bash -s" < $PROJECT_ROOT/delete_backups.sh "$REMOTE_DIR" "$RETAIN_DATE"
-
-# execture the sync as a background job
-rsync $ARGS $SOURCE $SERVER:$DEST & job=$!
-
-# display a spinner while rsync job is running
 printf "Working...  "
 while kill -0 $job 2>/dev/null ; do
     for s in / - \\ \|; do
@@ -54,3 +28,4 @@ while kill -0 $job 2>/dev/null ; do
     done
 done
 printf "\b  \n"
+
